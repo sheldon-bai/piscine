eef3e58f5a5570d1eb3e66e9e3eedf09eddf7b3f
sheldon-bai <sheldon.bai1989@gmail.com>
2017-01-22 17:03:26 -0800

    add d02

---
+++ d02/ex04/ft_print_comb.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xbai <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/12 19:05:54 by xbai              #+#    #+#             */
+/*   Updated: 2017/01/22 13:48:07 by xbai             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_printf(char a, char b, char c)
+{
+	ft_putchar(a);
+	ft_putchar(b);
+	ft_putchar(c);
+	if (a == '7' && b == '8' && c == '9')
+		return ;
+	else
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
+
+void	ft_print_comb(void)
+{
+	char a;
+	char b;
+	char c;
+
+	a = '0';
+	while (a <= '7')
+	{
+		b = a + 1;
+		while (b <= '8')
+		{
+			c = b + 1;
+			while (c <= '9')
+			{
+				ft_printf(a, b, c);
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+}
